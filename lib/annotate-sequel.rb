ea945e4068029ccd5d99203a26a6f6864735f118
Kenny Meyer <knny.myer@gmail.com>
2013-11-10 13:10:03 -0300

    Cut new release

--- lib/annotate-sequel.rb
+++ lib/annotate-sequel.rb
@@ -1,6 +1,28 @@
 $:.unshift(File.dirname(__FILE__))
 
-class AnnotateSequel
-  require 'annotate_sequel/version'
-  require 'annotate_sequel/model'
+require 'annotate_sequel/version'
+require 'annotate_sequel/model'
+
+module Annotate
+  def self.loaded_tasks=(val); @loaded_tasks = val; end
+  def self.loaded_tasks; return @loaded_tasks; end
+
+  def self.load_tasks
+    return if(self.loaded_tasks)
+    self.loaded_tasks = true
+    Dir[File.join(File.dirname(__FILE__), 'tasks', '**/*.rake')].each { |rake| load rake }
+  end
+
+  def self.bootstrap_rake
+    if File.exists?("./Rakefile")
+      load "./Rakefile"
+    end
+    Rake::Task[:environment].invoke rescue nil
+    self.load_tasks
+  end
+
+  def self.eager_load
+    klass = Rails::Application.send(:subclasses).first
+    klass.eager_load!
+  end
 end
--- lib/annotate_sequel/version.rb
+++ lib/annotate_sequel/version.rb
@@ -3,7 +3,7 @@ class AnnotateSequel
     MAJOR = 1
     MINOR = 0
     PATCH = 0
-    BUILD = 'pre1'
+    BUILD = 'pre2'
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end

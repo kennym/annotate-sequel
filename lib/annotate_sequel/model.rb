2e095dc91a6c90c29091f566dfe1f5e6d9bf5b5b
Kenny Meyer <knny.myer@gmail.com>
2013-11-14 15:00:22 -0300

    Add missing underscore method

--- lib/annotate_sequel/model.rb
+++ lib/annotate_sequel/model.rb
@@ -120,6 +120,13 @@ class AnnotateSequel
           puts "Unable to annotate #{file}: #{e.message}"
         end
       end
+
+      private
+
+      def underscore(word)
+        word.gsub(/::/, '/').gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+          gsub(/([a-z\d])([A-Z])/,'\1_\2').tr("-", "_").downcase
+      end
     end
   end
 end
